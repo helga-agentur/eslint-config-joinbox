7bf381f4b6a4c85944b4a640f9b423fc2248b6ab
Andi <andi@joinbox.ch>
2023-07-06 17:47:48 +0200

    build: update packages and adjust test file
    - update eslint
    - update eslint-config-airbnb-base
    - add @babel/eslint-parser and respective configs
    - adjust test file to our new rules

--- rules/joinbox-custom.js
+++ rules/joinbox-custom.js
@@ -1,10 +1,11 @@
 module.exports = {
     rules: {
-        // requires that file extentions are used in import statements, exept for package names
-        'import/extensions': ['error', 'always', { ignorePackages: true }],
+        // we use indentation of 4 spaces – just because we are used to
+        indent: ['error', 4],
 
-        // do not require empty lines between single line class members, such as private
+        // Do not require empty lines between single line class members, such as private
         // properties which are defined at the beginning of the class.
+        // The Airbnb JS Style Guide does not cover private properties.
         'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
 
         // omit logs in production code but allow warnings and errors
